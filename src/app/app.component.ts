3006c173e8e2451ae4783555614e1d548cb6271d
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2025-02-26 20:51:45 -0300

    Início do desenvolvimento do card principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MenuBarComponent,MenuTitleComponent,BigCardComponent,SmallCardComponent],
+  imports: [RouterOutlet,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
