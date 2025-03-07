a6cee5e1baad740dcf8da8eb0e1bbe60b9f8158b
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2025-03-07 10:49:22 -0300

    Criar component de conteúdo das páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './components/content-component/content-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:HomeComponent
+    },
+    {
+        path:"content",
+        component:ContentComponent
+    }
+];
