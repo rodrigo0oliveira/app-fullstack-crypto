2664de6af95b7cea5be0aa7c7d93014558825c55
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2025-03-26 19:30:44 -0300

    feature: iniciar desenvolvimento do microserviço de segurança

---
+++ backend-java/blog-security/src/test/java/br/com/backend/blog_security/BlogSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.blog_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
