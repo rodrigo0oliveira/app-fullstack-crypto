d640a6de9b9a2878ea9a00a72aacec2506463954
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2025-04-08 21:19:22 -0300

    Criar serviços e models de login do usuário

--- front-angular/src/app/pages/login/login.component.ts
+++ front-angular/src/app/pages/login/login.component.ts
@@ -3,6 +3,9 @@ import { DefaultLayoutLoginComponent } from '../../components/default-layout-log
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
+import { LoginRequired } from '../../models/LoginRequired';
+import { LoginService } from '../../services/login.service';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -12,17 +15,42 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
+  loginRequired:LoginRequired = {
+    userName:"",
+    password:""
+  }
+
   loginForm!:FormGroup;
 
-  constructor(private router:Router){
+  constructor(private router:Router,private loginService:LoginService){
     this.loginForm = new FormGroup({
-      email: new FormControl("",[Validators.required,Validators.email]),
+      username: new FormControl("",[Validators.required,Validators.email]),
       password: new FormControl("",[Validators.required,Validators.minLength(8)])
     })
   }
 
   submit(){
-    console.log(this.loginForm.value);
+    const username:string = this.loginForm.value.username;
+    const password:string = this.loginForm.value.password;
+
+    this.loginRequired = {
+      userName:username,
+      password:password
+    }
+
+    this.loginService.login(this.loginRequired).subscribe({
+      next:(response:HttpResponse<any>)=>{
+        const status = response.status;
+        if(status!=200){
+          console.log("Erro ao realizar o login");
+        }else{
+          console.log(response.body)
+        }
+      }
+    })
+
+
+
   }
 
   navigate(route:string){
