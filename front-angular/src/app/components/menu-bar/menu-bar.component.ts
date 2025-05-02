9caecc86d14b8cb7042fcdb8d68fe94c079067be
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2025-05-01 21:53:47 -0300

    feature : adiciona elementos dasyui ao projeto e deleta arquivos css que não serão mais utilizados

--- front-angular/src/app/components/menu-bar/menu-bar.component.ts
+++ front-angular/src/app/components/menu-bar/menu-bar.component.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
   selector: 'app-menu-bar',
   imports: [RouterModule],
   templateUrl: './menu-bar.component.html',
-  styleUrl: './menu-bar.component.css'
 })
 export class MenuBarComponent {
 
--- front-angular/src/app/pages/home/home.component.ts
+++ front-angular/src/app/pages/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';7
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 import { BigCardComponent } from '../../components/big-card/big-card.component';
-import { SmallCardComponent } from "../../components/small-card/small-card.component";
 import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent,MenuBarComponent],
+  imports: [MenuTitleComponent, BigCardComponent,MenuBarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+
 })
 export class HomeComponent{
 }
